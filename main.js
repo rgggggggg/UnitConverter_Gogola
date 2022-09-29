0fade67f098d0b560cd5c6429f420a44b17f9b62
rgggggggg <57524038+rgggggggg@users.noreply.github.com>
2022-09-29 15:50:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,20 @@ var num = document.getElementById('num');
 var result = document.getElementById('result');
 var numType = document.getElementById('numType');
 var resultType = document.getElementById('resultType');
-var inputTypeValue,resultTypeValue;
+var numTypeValue,resultTypeValue;
 
 num.addEventListener("keyup",myResult);
-inputType.addEventListener("change",myResult);
+numType.addEventListener("change",myResult);
 resultType.addEventListener("change",myResult);
 
 numTypeValue = numType.value;
 resultTypeValue = resultType.value;
 
 function myResult(){
+
     numTypeValue = numType.value;
     resultTypeValue = resultType.value;
+
+    result.value = num.value;
 }
+
